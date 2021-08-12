dfc0387c6a573bc9539554e55a11db1bb553895a
JS <joonseok.hwang@pm.me>
2021-08-12 14:31:54 +0900

    로그인 페이지 Form 추가

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../store";
 import {
@@ -24,7 +24,8 @@ export default function Home() {
   const icon = useSelector((state: RootState) => state.icon);
   const appName = useSelector((state: RootState) => state.system.appName);
 
-  function togglePasswordShow() {
+  function togglePasswordShow(event: FormEvent) {
+    event.preventDefault();
     setPasswordShow(!passwordShow);
   }
 
@@ -36,38 +37,46 @@ export default function Home() {
 
   return (
     <Center w={"100%"} h={"100%"}>
-      <Stack spacing={5} p={10} borderWidth={1} rounded="md" boxShadow="xl">
-        <Heading size="md">{appName}에 오신 것을 환영합니다.</Heading>
+      <form action="" onSubmit={login}>
+        <Stack spacing={5} p={10} borderWidth={1} rounded="md" boxShadow="xl">
+          <Heading size="md">{appName}에 오신 것을 환영합니다.</Heading>
 
-        <InputGroup>
-          <InputLeftElement
-            pointerEvents="none"
-            children={<Icon as={icon.id} />}
-          />
-          <Input placeholder="아이디" />
-        </InputGroup>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Icon as={icon.id} />}
+            />
+            <Input
+              placeholder="아이디"
+              autoComplete="username"
+              isRequired={true}
+            />
+          </InputGroup>
 
-        <InputGroup>
-          <InputLeftElement
-            pointerEvents="none"
-            children={<Icon as={icon.password} />}
-          />
-          <Input
-            type={passwordShow ? "text" : "password"}
-            pr="4.5rem"
-            placeholder="비밀번호"
-          />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={togglePasswordShow}>
-              {passwordShow ? "숨기기" : "보기"}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Icon as={icon.password} />}
+            />
+            <Input
+              type={passwordShow ? "text" : "password"}
+              placeholder="비밀번호"
+              autoComplete="current-password"
+              pr="4.5rem"
+              isRequired={true}
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={togglePasswordShow}>
+                {passwordShow ? "숨기기" : "보기"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
 
-        <Button onClick={login} colorScheme="blue">
-          로그인
-        </Button>
-      </Stack>
+          <Button type="submit" colorScheme="blue">
+            로그인
+          </Button>
+        </Stack>
+      </form>
 
       <Tooltip
         hasArrow
