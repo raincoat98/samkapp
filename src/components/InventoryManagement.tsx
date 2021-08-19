4bcf8647ed10645896caac280cd70c794ca2a6da
JS <joonseok.hwang@pm.me>
2021-08-19 21:26:52 +0900

    테이블 삭제 기능 추가, 로딩 스피너 추가

--- src/components/InventoryManagement.tsx
+++ src/components/InventoryManagement.tsx
@@ -3,9 +3,19 @@ import { useSelector } from "react-redux";
 import { RootState } from "store";
 import PageContainer from "components/frames/PageContainer";
 import TableComponent from "components/frames/TableComponent";
-import { ButtonGroup, Button, Tabs, TabList, Tab } from "@chakra-ui/react";
+import SpinnerComponent from "components/frames/SpinnerComponent";
+import {
+  useDisclosure,
+  ButtonGroup,
+  Button,
+  Tabs,
+  TabList,
+  Tab,
+} from "@chakra-ui/react";
 
 export default function InventoryManagement() {
+  const spinnerDisclosure = useDisclosure();
+
   const realmApp = useSelector((state: RootState) => state.realm.app);
   const [productTypes, setProductTypes] = React.useState<any[]>([]);
   const [products, setProducts] = React.useState<any[]>([]);
@@ -51,15 +61,39 @@ export default function InventoryManagement() {
     init();
 
     async function init() {
+      spinnerDisclosure.onOpen();
+
       if (productTypeCollection)
         await setProductTypes(await productTypeCollection.find());
       if (productCollection) await setProducts(await productCollection.find());
       console.log("updated");
+
+      spinnerDisclosure.onClose();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  async function deleteAll() {
-    // realmApp?.currentUser?.functions.delete_product({ name: "조판지" });
+  async function deleteSelected() {
+    spinnerDisclosure.onOpen();
+
+    const selectedProductIds: string[] = [];
+    const rowsById = mainTable.tableInstance.rowsById;
+    // @ts-ignore
+    const selectedRowIds = mainTable.tableInstance.state.selectedRowIds;
+
+    for (const key in selectedRowIds) {
+      // @ts-ignore
+      selectedProductIds.push(rowsById[key].original._id);
+    }
+
+    for (let i = 0; i < selectedProductIds.length; i++) {
+      await realmApp?.currentUser?.functions
+        .delete_product({ _id: selectedProductIds[i] })
+        .then((result) => {
+          console.log(result);
+        });
+    }
+    spinnerDisclosure.onClose();
   }
 
   async function onTabChange(tab: number) {
@@ -74,25 +108,32 @@ export default function InventoryManagement() {
   }
 
   return (
-    <PageContainer
-      title={"재고 관리"}
-      headerChildren={
-        <ButtonGroup spacing="3">
-          <Button variant="outline" colorScheme="red" onClick={deleteAll}>
-            삭제
-          </Button>
-        </ButtonGroup>
-      }
-    >
-      <Tabs onChange={onTabChange} isFitted>
-        <TabList>
-          <Tab>{"전체"}</Tab>
-          {productTypes.map((type, index) => (
-            <Tab key={index}>{type.name}</Tab>
-          ))}
-        </TabList>
-      </Tabs>
-      {mainTable.component}
-    </PageContainer>
+    <>
+      <SpinnerComponent isOpen={spinnerDisclosure.isOpen} />
+      <PageContainer
+        title={"재고 관리"}
+        headerChildren={
+          <ButtonGroup spacing="3">
+            <Button
+              variant="outline"
+              colorScheme="red"
+              onClick={deleteSelected}
+            >
+              삭제
+            </Button>
+          </ButtonGroup>
+        }
+      >
+        <Tabs onChange={onTabChange} isFitted>
+          <TabList>
+            <Tab>{"전체"}</Tab>
+            {productTypes.map((type, index) => (
+              <Tab key={index}>{type.name}</Tab>
+            ))}
+          </TabList>
+        </Tabs>
+        {mainTable.component}
+      </PageContainer>
+    </>
   );
 }
