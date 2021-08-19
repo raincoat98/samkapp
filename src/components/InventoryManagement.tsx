599ea3049612a19819abc900a00777436795a48c
JS <joonseok.hwang@pm.me>
2021-08-19 12:54:47 +0900

    테이블 마테리얼 디자인으로 변경

--- src/components/InventoryManagement.tsx
+++ src/components/InventoryManagement.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
 import PageContainer from "components/frames/PageContainer";
-import TableComponent from "components/frames/TableComponent";
-import { ButtonGroup, Button, Tabs, TabList, Tab } from "@chakra-ui/react";
+import { ButtonGroup, Button } from "@chakra-ui/react";
+import { DataGrid } from "@material-ui/data-grid";
 
 export default function InventoryManagement() {
-  const [isAllLoaded, setIsAllLoaded] = React.useState(false);
-
   const realmApp = useSelector((state: RootState) => state.realm.app);
   const [productTypes, setProductTypes] = React.useState<any[]>([]);
   const [products, setProducts] = React.useState<any[]>([]);
+  const [selectedProducts, setSelectedProducts] = React.useState<any[]>([]);
 
   const mongodb = realmApp?.currentUser?.mongoClient("mongodb-atlas");
   const productTypeCollection = mongodb
@@ -18,65 +17,58 @@ export default function InventoryManagement() {
     .collection("product_type");
   const productCollection = mongodb?.db("database").collection("product");
 
-  // 테이블
-  const mainTable = TableComponent({
-    columns: [
-      {
-        Header: "제품명",
-        accessor: "name",
-      },
-      {
-        Header: "규격",
-        accessor: "standard",
-      },
-      {
-        Header: "두께",
-        accessor: "thickness",
-      },
-      {
-        Header: "폭",
-        accessor: "width",
-      },
-      {
-        Header: "현재고",
-        accessor: "stock",
-      },
-      {
-        Header: "비고",
-        accessor: "note",
-      },
-    ],
-    data: products,
-  });
-
   React.useEffect(() => {
-    try {
-      if (!isAllLoaded) init();
-    } catch (error) {}
-    async function init() {
+    find();
+    // watchStart();
+
+    async function find() {
+      console.log("find start");
       if (productTypeCollection)
         await setProductTypes(await productTypeCollection.find());
-      if (productCollection) {
-        await setProducts(await productCollection.find());
-      }
-      setIsAllLoaded(true);
-      console.log("updated");
+      if (productCollection) await setProducts(await productCollection.find());
     }
-  }, [isAllLoaded, productCollection, productTypeCollection]);
 
-  async function deleteAll() {
-    // realmApp?.currentUser?.functions.delete_product({ name: "조판지" });
+    // async function watchStart() {
+    //   console.log("watch start");
+    //   for await (const change of productCollectionWatcher) {
+    //     console.log(change);
+    //     switch (change.operationType) {
+    //       case "insert": {
+    //         const { documentKey, fullDocument } = change;
+    //         console.log(`new document with _id: ${documentKey}`, fullDocument);
+    //         break;
+    //       }
+    //       case "update": {
+    //         const { documentKey, fullDocument } = change;
+    //         console.log(`updated document: ${documentKey}`, fullDocument);
+    //         break;
+    //       }
+    //       case "replace": {
+    //         const { documentKey, fullDocument } = change;
+    //         console.log(`replaced document: ${documentKey}`, fullDocument);
+    //         break;
+    //       }
+    //       case "delete": {
+    //         const { documentKey } = change;
+    //         console.log(`deleted document: ${documentKey}`);
+    //         break;
+    //       }
+    //     }
+    //   }
+    // }
+  }, []);
+
+  async function deleteSelected() {
+    for (let i = 0; i < selectedProducts.length; i++) {
+      realmApp?.currentUser?.functions
+        .delete_product({ _id: selectedProducts[i] })
+        .then((result) => console.log(result))
+        .catch((err) => console.error(`Delete failed with error: ${err}`));
+    }
   }
 
-  async function onTabChange(tab: number) {
-    const type = productTypes[--tab];
-    if (type) {
-      // @ts-ignore
-      mainTable.tableInstance.setAllFilters([{ id: "name", value: type.name }]);
-    } else {
-      // @ts-ignore
-      mainTable.tableInstance.setAllFilters([]);
-    }
+  function onSelectionModelChange(selected: any) {
+    setSelectedProducts(selected);
   }
 
   return (
@@ -84,21 +76,66 @@ export default function InventoryManagement() {
       title={"재고 관리"}
       headerChildren={
         <ButtonGroup spacing="3">
-          <Button variant="outline" colorScheme="red" onClick={deleteAll}>
+          <Button variant="outline" colorScheme="red" onClick={deleteSelected}>
             삭제
           </Button>
         </ButtonGroup>
       }
     >
-      <Tabs onChange={onTabChange} isFitted>
-        <TabList>
-          <Tab>{"전체"}</Tab>
-          {productTypes.map((type, index) => (
-            <Tab key={index}>{type.name}</Tab>
-          ))}
-        </TabList>
-      </Tabs>
-      {mainTable.component}
+      <DataGrid
+        autoHeight={true}
+        pagination
+        checkboxSelection
+        disableSelectionOnClick
+        disableColumnSelector
+        onSelectionModelChange={onSelectionModelChange}
+        getRowId={(r) => r._id}
+        rows={products}
+        columns={[
+          {
+            headerName: "제품명",
+            field: "name",
+            flex: 1,
+            headerAlign: "center",
+            align: "center",
+          },
+          {
+            headerName: "규격",
+            field: "standard",
+            flex: 1,
+            headerAlign: "center",
+            align: "center",
+          },
+          {
+            headerName: "두께",
+            field: "thickness",
+            flex: 1,
+            headerAlign: "center",
+            align: "center",
+          },
+          {
+            headerName: "폭",
+            field: "width",
+            flex: 1,
+            headerAlign: "center",
+          },
+          {
+            headerName: "현재고",
+            field: "stock",
+            flex: 1,
+            headerAlign: "center",
+            align: "center",
+            type: "number",
+          },
+          {
+            headerName: "비고",
+            field: "note",
+            flex: 1,
+            headerAlign: "center",
+            align: "center",
+          },
+        ]}
+      />
     </PageContainer>
   );
 }
