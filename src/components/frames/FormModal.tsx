75aa1401ad6ec563c6ef3cdcb906d2c34715c18a
JS <joonseok.hwang@pm.me>
2021-08-25 20:08:28 +0900

    폼모달 최적화

--- src/components/frames/FormModal.tsx
+++ src/components/frames/FormModal.tsx
@@ -2,24 +2,26 @@ import React from "react";
 import Moment from "moment";
 import ModalComponent from "components/frames/ModalComponent";
 import {
+  Box,
+  Tag,
   Stack,
   Button,
   Input,
+  Switch,
   ButtonGroup,
   FormControl,
   FormLabel,
   Divider,
-  InputProps,
   ModalProps,
 } from "@chakra-ui/react";
 
 export type modeType = string | "insert" | "update";
-export type inputType = {
+export type formItem = {
   key: string;
-  type: string;
-  inputOptions: InputProps;
-  defaultValue: any;
-  isRequired: boolean;
+  element?: JSX.Element;
+  isDisabled?: boolean;
+  isRequired?: boolean;
+  isInline?: boolean;
 };
 
 export default function InventoryModalComponent(
@@ -30,151 +32,180 @@ export default function InventoryModalComponent(
     onChange: Function;
   }
 ) {
+  const [editedDocument, setEditedDocument] = React.useState<
+    Record<string, any>
+  >({});
   const { onChange, initialValue, schmea, mode } = props;
   const formRef = React.useRef<HTMLFormElement>(null);
-  const inputList: inputType[] = [];
-  const disabledInputList: inputType[] = [];
+  const formItemList: formItem[] = [];
+  const disabledFormItemList: formItem[] = [];
 
   for (const key in schmea.properties) {
-    let isRequired = true;
     let type = schmea.properties[key];
-    const inputOptions: InputProps = {};
-    let defaultValue = initialValue ? initialValue[key] : "";
+    let defaultValue = initialValue ? initialValue[key] : undefined;
+    let element;
 
+    // ? 로 끝나는 것은 필수값이 아님
+    const isRequired = type.endsWith("?") ? false : true;
+    if (type.endsWith("?")) {
+      type = type.replaceAll("?", "");
+    }
+
+    // key가 _로 시작하는 값은 유저가 수정할 수 없는 값
     if (!key.startsWith("_")) {
-      // ? 로 끝나는 것은 필수값이 아님
-      if (type.endsWith("?")) {
-        isRequired = false;
-        type = type.replaceAll("?", "");
-      }
+      let isInline = false;
 
       switch (type) {
         case "string": {
-          inputOptions.type = "text";
+          element = (
+            <Input
+              type="text"
+              defaultValue={defaultValue}
+              onChange={(event) => {
+                onEdited({ key, value: event.target.value });
+              }}
+            />
+          );
           break;
         }
         case "int": {
-          inputOptions.type = "number";
+          element = (
+            <Input
+              type="number"
+              defaultValue={defaultValue}
+              onChange={(event) => {
+                onEdited({ key, value: event.target.value });
+              }}
+            />
+          );
           break;
         }
         case "date": {
-          inputOptions.type = "date";
+          element = (
+            <Input
+              type="date"
+              defaultValue={defaultValue}
+              onChange={(event) => {
+                onEdited({ key, value: event.target.value });
+              }}
+            />
+          );
           break;
         }
+        case "bool": {
+          const checked = defaultValue ? true : false;
+          element = (
+            <Switch
+              defaultChecked={checked}
+              onChange={(event) => {
+                onEdited({ key, value: event.target.checked });
+              }}
+            />
+          );
+          isInline = true;
+          break;
+        }
+        case "objectId": {
+          continue;
+        }
+        default: {
+          console.log("알 수 없는 타입: ", type);
+        }
       }
-      inputList.push({ key, type, inputOptions, defaultValue, isRequired });
+
+      formItemList.push({ key, element, isRequired, isInline });
     } else {
       // 데이터 기본 키와 같을 경우 넣지 않음
       if (key === schmea.primaryKey) continue;
-      inputOptions.type = "text";
-      inputOptions.isDisabled = true;
 
-      if (type === "date") {
-        defaultValue = Moment(defaultValue)
-          .local()
-          .format("YYYY년 MM월 DD일 a h시 m분");
+      const name = key.replace("_", "");
+
+      switch (type) {
+        case "date": {
+          // 수정 불가능한 date 값
+          element = (
+            <Tag>
+              {`${name}: `}
+              {Moment(defaultValue)
+                .local()
+                .format("YYYY년 MM월 DD일 a h시 m분")}
+            </Tag>
+          );
+        }
       }
 
-      disabledInputList.push({
+      disabledFormItemList.push({
         key,
-        type,
-        inputOptions,
-        defaultValue,
-        isRequired,
+        element,
+        // 수정 불가
+        isDisabled: true,
+        isInline: true,
       });
+    }
+
+    if (!key.startsWith("_")) {
+      // formItemList.push({ key, element, isRequired, isDisabled, isInline });
+    } else {
     }
   }
 
-  function onSave() {
-    const doc: Record<string, any> = {};
+  function onEdited(props: { key: string; value: any }) {
+    setEditedDocument({ ...editedDocument, ...{ [props.key]: props.value } });
+  }
 
-    for (const key in schmea.properties) {
-      // form 에서 값 가져오기
-      if (formRef.current) {
-        const inputElement = formRef.current.querySelector<HTMLInputElement>(
-          `#${key}`
-        );
-
-        if (inputElement) {
-          if (inputElement.disabled) continue;
-
-          let inputElementValue: string | number = inputElement.value;
-          const inputElementType = inputElement.type;
-
-          switch (inputElementType) {
-            case "number": {
-              inputElementValue = Number(inputElementValue);
-              break;
-            }
-          }
-
-          // inputElementValue가 0일경우 false가 나오므로 undefined 비교 사용
-          if (inputElementValue !== undefined) {
-            if (!initialValue || initialValue[key] !== inputElementValue) {
-              doc[key] = inputElementValue;
-            }
-          }
-        }
-      }
-    }
-
-    onChange({
-      type: mode,
-      document: doc,
-      initialValue,
-    });
+  function onClose() {
+    setEditedDocument({});
+    props.onClose();
   }
 
   return (
     <ModalComponent
       isOpen={props.isOpen}
-      onClose={props.onClose}
+      onClose={onClose}
       isCentered={true}
       size="2xl"
       scrollBehavior="inside"
       headerChildren={mode === "insert" ? "추가" : "수정"}
       footerChildren={
         <ButtonGroup>
-          <Button type="submit" onClick={onSave} colorScheme="blue">
+          <Button
+            type="submit"
+            onClick={() => {
+              onChange({
+                type: mode,
+                document: editedDocument,
+                initialValue,
+              });
+            }}
+            isDisabled={Object.keys(editedDocument).length === 0}
+            colorScheme="blue"
+          >
             저장
           </Button>
-          <Button onClick={props.onClose}>닫기</Button>
+          <Button onClick={onClose}>닫기</Button>
         </ButtonGroup>
       }
     >
       <form action="" ref={formRef}>
         <Stack>
-          {inputList.map((schmea, index) => (
+          {formItemList.map((formItem, index) => (
             <FormControl
-              id={schmea.key}
-              isRequired={schmea.isRequired}
+              id={formItem.key}
+              isRequired={formItem.isRequired}
               key={index}
+              display={formItem.isInline ? "flex" : ""}
+              alignItems="center"
             >
-              <FormLabel>{schmea.key}</FormLabel>
-              <Input
-                type={schmea.inputOptions.type}
-                isDisabled={schmea.inputOptions.isDisabled}
-                defaultValue={schmea.defaultValue}
-              />
+              <FormLabel>{formItem.key}</FormLabel>
+              {formItem.element}
             </FormControl>
           ))}
 
-          <Divider py={2} />
+          <Divider />
 
-          {disabledInputList.map((schmea, index) => (
-            <FormControl
-              id={schmea.key}
-              isDisabled={schmea.inputOptions.isDisabled}
-              pt={2}
-              key={index}
-            >
-              <FormLabel>{schmea.key}</FormLabel>
-              <Input
-                type={schmea.inputOptions.type}
-                defaultValue={schmea.defaultValue}
-              />
-            </FormControl>
-          ))}
+          {disabledFormItemList.map((schmea, index) =>
+            schmea.element ? <Box key={index}>{schmea.element}</Box> : ""
+          )}
         </Stack>
       </form>
     </ModalComponent>
