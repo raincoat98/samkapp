1c1ad6ebb38d78a6a397ba8fc1c602fc2d9752d5
JS <joonseok.hwang@pm.me>
2021-09-07 17:36:19 +0900

    Revert "앱아이디 남아있던 값 제거"
    This reverts commit 63fd47c70c44b45a5528ffe3980722d6fdf61578.

--- src/store/realm.ts
+++ src/store/realm.ts
@@ -4,6 +4,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 const APP_ID = "samkapp-dzval";
 
 export type SystemState = {
+  appId: string;
   app: RealmWeb.App;
   user: RealmWeb.User | null;
   readonlySchemaKeyList: string[];
@@ -11,6 +12,7 @@ export type SystemState = {
 };
 
 const initialState: SystemState = {
+  appId: "samkapp-dzval",
   app: new RealmWeb.App({ id: APP_ID }),
   user: null,
   readonlySchemaKeyList: ["create_by", "create_dttm", "save_by", "save_dttm"],
