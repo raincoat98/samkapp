f856b78ddf87eacab15c62a6a587fb005c086fbc
JS <joonseok.hwang@pm.me>
2021-09-07 13:20:12 +0900

    realm init 부분 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "./store";
-import * as RealmWeb from "realm-web";
 import Login from "components/Login";
 import AppRouter from "components/AppRouter";
 import SpinnerComponent from "components/base/SpinnerComponent";
@@ -13,20 +11,7 @@ export default function App() {
     type: "system/closeProgress",
   });
 
-  const realmAppId = useSelector((state: RootState) => state.realm.appId);
   const realmAppUser = useSelector((state: RootState) => state.realm.user);
-
-  React.useEffect(() => {
-    init();
-
-    async function init() {
-      const realmApp = new RealmWeb.App({ id: realmAppId });
-      dispatch({
-        type: "realm/init",
-        payload: realmApp,
-      });
-    }
-  }, [dispatch, realmAppId]);
 
   return (
     <div
