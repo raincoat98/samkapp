58c5304257f930fa8132b2ff8c06947fc19621f7
JS <joonseok.hwang@pm.me>
2021-09-07 12:01:53 +0900

    개발 환경일 때에만 리덕스 로그가 표시되게 수정

--- src/index.tsx
+++ src/index.tsx
@@ -19,12 +19,24 @@ import { createStore, applyMiddleware } from "redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import rootReducer from "store";
-import logger from "redux-logger";
 
 // Moment (시간 관리 라이브러리) - 한국어 지원
 import "moment/locale/ko";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+// redux 미들웨어 적용
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  const { createLogger } = require("redux-logger");
+
+  const logger = createLogger({
+    collapsed: true,
+  });
+
+  middlewares.push(logger);
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 const persistor = persistStore(store);
 
 ReactDOM.render(
