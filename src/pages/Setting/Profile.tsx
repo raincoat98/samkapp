1d8291f9c2fb69f410fbff7a508f532d37fce2e0
JS <joonseok.hwang@pm.me>
2021-09-13 11:01:51 +0900

    Clean setting page (#13)
    * 설정 페이지 미구현한 부분 제거
    
    * 프로필 정렬 및 패딩 변경

--- src/pages/Setting/Profile.tsx
+++ src/pages/Setting/Profile.tsx
@@ -9,7 +9,7 @@ export default function Profile() {
   const userName = useSelector((state: RootState) => state.realm.userName);
 
   return (
-    <Wrap>
+    <Wrap align="center" spacing={3}>
       <WrapItem>
         <Icon
           as={user}
@@ -19,14 +19,15 @@ export default function Profile() {
           backgroundColor="white"
           color="black"
           padding="3"
-          marginRight="3"
         />
       </WrapItem>
+
       <WrapItem>
         <Heading as="h4" size="md">
           {userName}
         </Heading>
       </WrapItem>
+
       <WrapItem>
         <Button onClick={() => dispatch(logout())}>로그아웃</Button>
       </WrapItem>
--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -5,17 +5,14 @@ import LicensesList from "./LicensesList";
 import {
   useDisclosure,
   useColorMode,
+  Stack,
   Box,
   Heading,
   FormControl,
   FormLabel,
-  FormHelperText,
-  Divider,
+  StackDivider,
   Button,
   Switch,
-  Select,
-  InputGroup,
-  Input,
   ButtonGroup,
 } from "@chakra-ui/react";
 
@@ -33,77 +30,39 @@ export default function Setting() {
         </ButtonGroup>
       }
     >
-      <Box p={3}>
-        <Heading as="h4" size="md">
-          계정
-        </Heading>
-        <br />
-        <form>
-          <Profile />
+      <Stack divider={<StackDivider />} spacing={3} p={3}>
+        <Box>
+          <Heading as="h4" size="md">
+            계정
+          </Heading>
           <br />
+          <Profile />
+        </Box>
 
-          <FormControl>
-            <FormLabel>비밀번호 변경</FormLabel>
-            <InputGroup>
-              <Input
-                id={"current-password"}
-                placeholder="현재 비밀번호"
-                mr={3}
-              />
-              <Input id={"new-password"} placeholder="변경할 비밀번호" mr={3} />
-              <Button type="submit">확인</Button>
-            </InputGroup>
-            <FormHelperText>비밀번호 변경시에만 입력해주세요.</FormHelperText>
+        <Box>
+          <Heading as="h4" size="md">
+            시스템
+          </Heading>
+          <br />
+          <FormControl display="flex" alignItems="center">
+            <FormLabel>다크 모드</FormLabel>
+            <Switch
+              isChecked={colorMode === "dark"}
+              onChange={toggleColorMode}
+            />
           </FormControl>
-        </form>
-      </Box>
+        </Box>
 
-      <Divider />
-
-      <Box p={3}>
-        <Heading as="h4" size="md">
-          시스템
-        </Heading>
-        <br />
-        <FormControl display="flex" alignItems="center">
-          <FormLabel>다크 모드</FormLabel>
-          <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} />
-        </FormControl>
-        <Divider my={3} />
-        <FormControl>
-          <FormLabel>사용 언어</FormLabel>
-          <Select placeholder="시스템 언어">
-            <option value="option1">한국어</option>
-            <option value="option2">English</option>
-            <option value="option3">日本語</option>
-          </Select>
-        </FormControl>
-        <Divider my={3} />
-        <FormControl>
-          <FormLabel>업데이트</FormLabel>
-          <Select placeholder="자동">
-            <option value="option1">1</option>
-            <option value="option2">2</option>
-          </Select>
-        </FormControl>
-      </Box>
-
-      <Divider />
-
-      <Box p={3}>
-        <Heading as="h4" size="md">
-          기타
-        </Heading>
-        <br />
-        <Button>홈페이지</Button>
-        <Divider my={3} />
-        <Button>고객문의</Button>
-        <Divider my={3} />
-        <Button onClick={licenseModalDisclosure.onOpen}>
-          오픈소스 라이센스
-        </Button>
-      </Box>
-
+        <Box>
+          <Heading as="h4" size="md">
+            기타
+          </Heading>
+          <br />
+          <Button onClick={licenseModalDisclosure.onOpen}>
+            오픈소스 라이센스
+          </Button>
+        </Box>
+      </Stack>
       <ModalComponent
         headerChildren={"오픈소스 라이센스"}
         isOpen={licenseModalDisclosure.isOpen}
