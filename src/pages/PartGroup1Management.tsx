22ba01c5affb544f5ca4edce8056d4e7895ef1ff
JS <joonseok.hwang@pm.me>
2021-10-07 22:16:47 +0900

    대분류 & 중분류 관리 기본 아이디값 지정

--- src/pages/PartGroup1Management.tsx
+++ src/pages/PartGroup1Management.tsx
@@ -9,11 +9,24 @@ export default function PartGrou1Management() {
     (state: RootState) => state.realm.database[collectionName]
   );
 
+  // 인덱스 앞 0 붙이기 (ex 007)
+  let prefix = "";
+  for (let index = 0; index < 3 - data.length.toString().length; index++) {
+    prefix += "0";
+  }
+
   return (
     <Management
       title="중분류 관리"
       schema={part_group_1Schema}
       collectionName={collectionName}
+      formModalOptions={{
+        _id: {
+          autofill: {
+            value: `GR1_${prefix}${data.length}`,
+          },
+        },
+      }}
       tableProps={{ data }}
     />
   );
--- src/pages/PartGroup2Management.tsx
+++ src/pages/PartGroup2Management.tsx
@@ -9,11 +9,24 @@ export default function PartGrou2Management() {
     (state: RootState) => state.realm.database[collectionName]
   );
 
+  // 인덱스 앞 0 붙이기 (ex 007)
+  let prefix = "";
+  for (let index = 0; index < 3 - data.length.toString().length; index++) {
+    prefix += "0";
+  }
+
   return (
     <Management
       title="대분류 관리"
       schema={part_group_2Schema}
       collectionName={collectionName}
+      formModalOptions={{
+        _id: {
+          autofill: {
+            value: `GR2_${prefix}${data.length}`,
+          },
+        },
+      }}
       tableProps={{ data }}
     />
   );
