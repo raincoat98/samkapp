e1b390019a457f33e3539bb820b7deb959baf123
JS <joonseok.hwang@pm.me>
2021-11-10 17:42:54 +0900

    fix 항목 추가시에 기본키값이 안보이는 문제 수정
    항목 추가시에는 기본키를 보이게 하고 항목 수정일 때만 기본키값을 가림

--- src/components/Management/FormModal/index.tsx
+++ src/components/Management/FormModal/index.tsx
@@ -83,7 +83,7 @@ export default function FormModal(props: FormModalProps) {
     for (const key in props.schema.properties) {
       // 유저가 수정 못하는 값일 경우 다음으로
       if (
-        props.schema.primaryKey === key ||
+        (props.mode === "update" && props.schema.primaryKey === key) ||
         !!disabledSchemaKeyList.filter((disabledKey) => disabledKey === key)
           .length
       )
