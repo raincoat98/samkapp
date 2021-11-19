90febf009a4bb4e163cc7e3926e21625514f1773
JS <joonseok.hwang@pm.me>
2021-11-19 09:49:31 +0900

    Input Number 기본 최소값 0으로 설정

--- src/components/Input/InputNumber.tsx
+++ src/components/Input/InputNumber.tsx
@@ -3,12 +3,14 @@ import { Input } from "@chakra-ui/react";
 export default function InputNumber(props: {
   onChange: (value: number) => void;
   defaultValue?: number;
+  min?: number;
 }) {
   return (
     <Input
       type="number"
       onChange={(event) => props.onChange(event.target.valueAsNumber)}
       defaultValue={props.defaultValue}
+      min={props.min ?? 0}
     />
   );
 }
