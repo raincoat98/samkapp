a54c61f3101e0782fb2f1999767fcf08d3ae2074
JS <joonseok.hwang@pm.me>
2021-12-02 16:00:47 +0900

    외부 테이블 선택 인풋을 select 엘리먼트로 변경 (#83)
    * 인풋 전환
    
    * SearchPopover 컴포넌트 제거
    
    * fuse.js 패키지 제거 및 audit fix

--- src/components/Input/InputEnum.tsx
+++ src/components/Input/InputEnum.tsx
@@ -1,80 +1,27 @@
-import React from "react";
-import Fuse from "fuse.js";
-import { Box, Wrap, WrapItem, Input, Button } from "@chakra-ui/react";
+import { Select } from "@chakra-ui/react";
 
 export default function InputEnum(props: {
   enumList: any[];
   searchKey: string;
   displayKey?: string;
-  onChange: (selected: Fuse.FuseResult<any>) => void;
+  onChange: (selected: any) => void;
   defaultValue?: any;
 }) {
-  const inputEl = React.useRef<HTMLInputElement>(null);
-
-  const searchKeys: string[] = [props.searchKey];
-  if (props.displayKey) searchKeys.push(props.displayKey);
-
-  // 검색 라이브러리
-  const fuse = new Fuse(props.enumList, {
-    keys: searchKeys,
-    includeScore: true,
-  });
-
-  // 검색어
-  const [searchWord, setSearchWord] = React.useState("");
-
-  // 검색 결과
-  const [searchResults, setSearchResults] = React.useState<
-    Fuse.FuseResult<any>[]
-  >([]);
-
-  // 검색 결과 클릭시
-  function onSearchItemClick(searhData: any) {
-    if (inputEl.current) inputEl.current.value = searhData;
-    props.onChange(searhData);
-  }
-
   return (
-    <Box>
-      <Input
-        onChange={(event) => {
-          setSearchResults(fuse.search(event.target.value));
-          setSearchWord(event.target.value);
-        }}
-        defaultValue={props.defaultValue}
-        ref={inputEl}
-      />
-      <Wrap marginTop={2} padding={1} maxHeight={200} overflow="auto">
-        {searchWord === ""
-          ? //  검색어를 입력하지 않았을 때
-            props.enumList.map((enumItem, index) => (
-              <WrapItem key={index}>
-                <Button
-                  onClick={() => onSearchItemClick(enumItem[props.searchKey])}
-                  size="sm"
-                  colorScheme="blue"
-                >
-                  <Box>{enumItem[props.displayKey ?? props.searchKey]}</Box>
-                </Button>
-              </WrapItem>
-            ))
-          : // 검색어를 입력했을 때
-            searchResults.map((searchResult, index) => (
-              <WrapItem key={index}>
-                <Button
-                  onClick={() =>
-                    onSearchItemClick(searchResult.item[props.searchKey])
-                  }
-                  size="sm"
-                  colorScheme="blue"
-                >
-                  <Box>
-                    {searchResult.item[props.displayKey ?? props.searchKey]}
-                  </Box>
-                </Button>
-              </WrapItem>
-            ))}
-      </Wrap>
-    </Box>
+    <Select
+      onChange={(event) =>
+        props.onChange(
+          props.enumList[Number(event.target.value)][props.searchKey]
+        )
+      }
+      defaultValue={props.defaultValue}
+      placeholder="없음"
+    >
+      {props.enumList.map((enumItem, index) => (
+        <option value={index} key={index}>
+          {enumItem[props.displayKey ?? props.searchKey]}
+        </option>
+      ))}
+    </Select>
   );
 }
