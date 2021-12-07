84b29758e47ec375149a075e4eb9f2e503e98c18
WooSangWook <ww57403@naver.com>
2021-12-07 16:07:39 +0900

    품목 가격 api 추가 (#95)

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const transferOut = require("./routes/transferOut");
 const workOrder = require("./routes/workOrder");
 const group2 = require("./routes/group2");
 const partType = require("./routes/partType");
+const unit = require("./routes/unit");
 
 // Cross-Origin Resource Sharing(CORS) 오류 해결을 위해 사용
 const cors = require("cors");
@@ -21,6 +22,10 @@ app.use("/transfer-out", transferOut); // 출고
 app.use("/work-order", workOrder); // 작업지시
 app.use("/group2", group2); // 품목 분류
 app.use("/part-type", partType); // 품목 형태
+app.use("/unit", unit); // 품목 가격
+app.use("/warehouse", warehouse); // 창고
+app.use("/part", part); // 품목
+app.use("/customer", customer); // 거래처
 
 // server port 3002 할당
 // 클라이언트와 다른 번호로 충돌나지 않도록
