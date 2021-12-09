83658c2ec90eba5fccfc8b2e3b9f1e136504654a
WooSangWook <ww57403@naver.com>
2021-12-09 15:02:40 +0900

    출고 api 등록 로직 수정 (#100)

--- server/routes/transferOut.js
+++ server/routes/transferOut.js
@@ -27,14 +27,17 @@ router.get("/all", (req, res) => {
 });
 
 // 출고 등록
-// http://localhost:3002/transfer-out/all?transfer_type_id=OUT_SALES&part_id=1&warehouse_id=1
+// http://localhost:3002/transfer-out/create?transfer_type_id=OUT_SALES&plan_date=20211218&priorities=1&part_id=1&quantity=1001&warehouse_id=1&customer_id=1
 router.get("/create", (req, res) => {
-  const sql = "CALL usp_transfer_out_INS(?,?,?,?)";
+  const sql = "CALL usp_transfer_out_INS(?,?,?,?,?,?,?)";
   const params = [
     req.query["transfer_type_id"],
+    req.query["plan_date"],
+    req.query["priorities"],
     req.query["part_id"],
     req.query["quantity"],
     req.query["warehouse_id"],
+    req.query["customer_id"],
   ];
 
   connection.query(sql, params, function (error, results) {
