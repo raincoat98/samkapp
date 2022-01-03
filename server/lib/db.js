14039430913f2791c52ecbfa205f45658492c1db
JS <business.6ubj5@simplelogin.co>
2022-01-03 17:35:55 +0900

    Db config (#110)
    * DB 접속 설정 파일 분리
    
    * Update .gitignore

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,10 +1,12 @@
-var mysql = require("mysql"); // mysql 변수에 mysql 모듈을 할당
-var connection = mysql.createConnection({
+import DB_CONFIG from "./config";
+
+const mysql = require("mysql"); // mysql 변수에 mysql 모듈을 할당
+const connection = mysql.createConnection({
   //커넥션변수에 mysql변수에 있는 크리에이드커넥션 메소드를 호출(객체를 받음) 할당
-  host: "localhost", //host객체 - 마리아DB가 존재하는 서버의 주소
-  user: "esamter", //user객체 - 마리아DB의 계정
-  password: "esamter1!", //password객체 - 마리아DB 계정의 비밀번호
-  port: "3308",
-  database: "inv", //database객체 - 접속 후 사용할 DB명
+  host: DB_CONFIG.host, //host객체 - 마리아DB가 존재하는 서버의 주소
+  user: DB_CONFIG.user, //user객체 - 마리아DB의 계정
+  password: DB_CONFIG.password, //password객체 - 마리아DB 계정의 비밀번호
+  port: DB_CONFIG.port,
+  database: DB_CONFIG.database, //database객체 - 접속 후 사용할 DB명
 });
 module.exports = connection;
