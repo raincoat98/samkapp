18bc8ab9d2b6f62dd7ca61d97d1eb986ba83e790
JS <js_hwang@samter.onmicrosoft.com>
2022-02-15 13:43:38 +0900

    품목 명세서 번역 오류 수정

--- src/i18n/database/bill_of_materials.ts
+++ src/i18n/database/bill_of_materials.ts
@@ -5,7 +5,7 @@ const bill_of_materials = {
     product_id: "생산 품목 코드",
     assembly_id: "조립 품목 코드",
     unit_id: "생산 품목단위",
-    assembly_Qty: "품목 명세서 코드",
+    assembly_Qty: "조립 품목 수량",
     bom_level: "품목 명세서 레벨",
     start_date: "적용시작일",
     end_date: "적용종료일",
