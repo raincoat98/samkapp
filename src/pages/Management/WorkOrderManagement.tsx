c3d5e86cba1aeac692a83711d03f4e95875c6380
JS <js_hwang@samter.onmicrosoft.com>
2022-02-18 16:15:49 +0900

    작업 지시 페이지에 기간 만료 탭 추가

--- src/pages/Management/WorkOrderManagement.tsx
+++ src/pages/Management/WorkOrderManagement.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
 import Management from "components/Management/index";
 import { work_orderSchema } from "schema/work_order";
+import moment from "moment";
 
 export default function WorkOrderManagement() {
   const collectionName = "work_order";
-  const [tabIndex, setTabIndex] = React.useState(0);
-
+  const [tabIndex, setTabIndex] = useState(0);
   const data = useSelector(
     (state: RootState) => state.realm.database[collectionName]
   );
@@ -17,6 +17,10 @@ export default function WorkOrderManagement() {
     data.filter((item) => !item.status || item.status === 0),
     // 진행중 작업지시 목록
     data.filter((item) => item.status === 1),
+    // 작업 기간은 지나고, 미완료된 작업지시 목록
+    data.filter(
+      (item) => item.status !== 2 && moment(item.plan_date).isBefore(moment())
+    ),
     // 완료된 작업지시 목록
     data.filter((item) => item.status === 2),
   ];
@@ -28,7 +32,7 @@ export default function WorkOrderManagement() {
       tabProps={{
         tabGroups: [
           {
-            data: ["대기", "진행중", "완료"],
+            data: ["대기", "진행중", "기간 만료", "완료"],
             onTabChange: (props) =>
               props.index !== undefined && setTabIndex(props.index),
           },
