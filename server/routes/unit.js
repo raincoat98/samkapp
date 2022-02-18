bc3e806a2d6046fb93d4e598af39280bad92274f
frank9715 <evil9715@naver.com>
2022-02-18 16:11:07 +0900

    품목단위 프로시저 추가

--- server/routes/unit.js
+++ server/routes/unit.js
@@ -1,44 +1,48 @@
 const express = require("express");
 const router = express();
-const { runQuery } = require("./index");
+const { runProcedure } = require("./index");
 
 // 단위
 // 조회
 router.get("/all", (req, res) => {
-  const sql = "SELECT unit_id, unit_name_kor, unit_name_eng FROM tb_unit";
+  const sql = `CALL usp_unit_LST ()`;
 
-  runQuery(res, sql);
+  runProcedure(res, sql);
 });
 
 // 등록
 router.get("/create", (req, res) => {
-  const sql =
-    "INSERT INTO tb_unit " +
-    "(unit_id, unit_name_kor, unit_name_eng " +
-    "VALUES(?, ?, ?);";
+  const sql = `CALL usp_unit_INS (${new Array(3).fill("?").toString()})`;
   const params = [
     req.query["unit_id"],
     req.query["unit_name_kor"],
     req.query["unit_name_eng"],
   ];
 
-  runQuery(res, sql, params);
+  runProcedure(res, sql, params);
 });
+
 
 // 수정
 router.get("/update", (req, res) => {
-  res.status(501).send({
-    success: false,
-  });
-  console.log("미구현: 품목 단위 수정");
+  const sql = `CALL usp_unit_UPD (${new Array(3).fill("?").toString()})`;
+  const params = [
+    req.query["unit_id"],
+    req.query["unit_name_kor"],
+    req.query["unit_name_eng"],
+  ];
+
+  runProcedure(res, sql, params);
 });
 
 //  삭제
 router.delete("/delete", (req, res) => {
-  const sql = "DELETE FROM tb_unit WHERE unit_id=?";
-  const params = [req.query["unit_id"]];
+  const sql = `CALL usp_unit_DEL (${new Array(1).fill("?").toString()})`;
+  const params = [
+    req.query["unit_id"],
+  ];
 
-  runQuery(res, sql, params);
+  runProcedure(res, sql, params);
 });
 
 module.exports = router;
