570398ed31715535a144d0f6aeab01f6ac351b29
frank9715 <evil9715@naver.com>
2022-02-24 15:13:05 +0900

    품목명세서 프로시저 수정

--- server/routes/billOfMaterials.js
+++ server/routes/billOfMaterials.js
@@ -12,11 +12,10 @@ router.get("/all", (req, res) => {
 
 // 등록
 router.get("/create", (req, res) => {
-  const sql = `CALL usp_bom_INS (${new Array(7).fill("?").toString()})`;
+  const sql = `CALL usp_bom_INS (${new Array(6).fill("?").toString()})`;
   const params = [
     req.query["product_id"], // NOT NULL
     req.query["assembly_id"], // NOT NULL
-    req.query["unit_id"], // NOT NULL
     req.query["assembly_Qty"], // NOT NULL
     req.query["bom_level"],
     req.query["start_date"], // NOT NULL
@@ -28,12 +27,11 @@ router.get("/create", (req, res) => {
 
 // 수정
 router.get("/update", (req, res) => {
-  const sql = `CALL usp_bom_UPD (${new Array(8).fill("?").toString()})`;
+  const sql = `CALL usp_bom_UPD (${new Array(7).fill("?").toString()})`;
   const params = [
     req.query["bom_id"], // NOT NULL
     req.query["product_id"], // NOT NULL
     req.query["assembly_id"], // NOT NULL
-    req.query["unit_id"], // NOT NULL
     req.query["assembly_Qty"], // NOT NULL
     req.query["bom_level"],
     req.query["start_date"], // NOT NULL
@@ -45,10 +43,10 @@ router.get("/update", (req, res) => {
 
 // 삭제
 router.delete("/delete", (req, res) => {
-  res.status(501).send({
-    success: false,
-  });
-  console.log("미구현: 품목 명세서 삭제");
+  const sql = "CALL usp_bom_DEL(?)";
+  const params = [req.query["bom_id"]];
+
+  runProcedure(res, sql, params);
 });
 
 module.exports = router;
