49ef74bc539a1921a0b6e3fe885d7a7277af34c0
JS <js_hwang@samter.onmicrosoft.com>
2022-02-24 10:10:41 +0900

    생산지시 페이지에 탭 기능 추가

--- src/pages/Management/ProdOrder.tsx
+++ src/pages/Management/ProdOrder.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
+import moment from "moment";
 import Management from "components/Management/index";
 import { product_orderSchema } from "schema/product_order";
 
@@ -8,12 +10,55 @@ export default function ProductOrderManager() {
   const data = useSelector(
     (state: RootState) => state.realm.database[collectionName]
   );
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const waitingList = data.filter((item) => !item.status || item.status === 0);
+
+  const dataList = [
+    // 대기 및 미분류 작업지시 목록
+    {
+      name: "대기",
+      data: waitingList,
+      isDisabled: !waitingList.length,
+    },
+    // 진행중 작업지시 목록
+    {
+      name: "진행중",
+      data: data.filter((item) => item.status === 1),
+      isDisabled: !data.find((item) => item.status === 1),
+    },
+    // 작업 기간은 지나고, 미완료된 작업지시 목록
+    {
+      name: "기간 만료",
+      data: data.filter(
+        (item) => item.status !== 2 && moment(item.due_date).isBefore(moment())
+      ),
+      isDisabled: !data.find(
+        (item) => item.status !== 2 && moment(item.due_date).isBefore(moment())
+      ),
+    },
+    // 완료된 작업지시 목록
+    {
+      name: "완료",
+      data: data.filter((item) => item.status === 2),
+      isDisabled: !data.find((item) => item.status === 2),
+    },
+  ];
 
   return (
     <Management
       title="생산지시 현황"
       schema={product_orderSchema}
-      tableProps={{ data }}
+      tabProps={{
+        tab: {
+          data: dataList.map(({ name, isDisabled }) => {
+            return { name, isDisabled: isDisabled };
+          }),
+          onTabChange: (props) =>
+            props.index !== undefined && setTabIndex(props.index),
+        },
+      }}
+      tableProps={{ data: dataList[tabIndex].data }}
     />
   );
 }
