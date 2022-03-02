a98593a320da92efeae72c0b8af26f15ae6d68df
frank9715 <evil9715@naver.com>
2022-03-02 15:38:51 +0900

    창고 수정(쿼리->프로시저)

--- server/routes/warehouse.js
+++ server/routes/warehouse.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const router = express();
-const { runQuery } = require("./index");
+const { runProcedure } = require("./index");
 
 // 창고
 // 조회
 router.get("/all", (req, res) => {
-  const sql =
-    "SELECT warehouse_id, warehouse_name, rack_no, cell_no, row_no, use_yn " +
-    "FROM tb_warehouse";
+  const sql = `CALL usp_warehouse_LST ()`;
 
-  runQuery(res, sql, undefined);
+  runProcedure(res, sql);
 });
+
 
 // 등록
 router.get("/create", (req, res) => {
-  const sql =
-    "INSERT INTO tb_warehouse " +
-    "(warehouse_name, rack_no, cell_no, row_no, use_yn) " +
-    `VALUES(${new Array(5).fill("?").toString()})`;
+  const sql = `CALL usp_warehouse_INS (${new Array(5).fill("?").toString()})`;
   const params = [
     req.query["warehouse_name"],
     req.query["rack_no"],
@@ -26,34 +22,32 @@ router.get("/create", (req, res) => {
     req.query["use_yn"],
   ];
 
-  runQuery(res, sql, params);
+  runProcedure(res, sql, params);
 });
+
 
 // 수정
 router.get("/update", (req, res) => {
-  const sql =
-    "UPDATE tb_warehouse " +
-    "SET warehouse_name=?, rack_no=?, cell_no=?, row_no=?, use_yn=? " +
-    "WHERE warehouse_id=?";
-
+  const sql = `CALL usp_warehouse_UPD (${new Array(6).fill("?").toString()})`;
   const params = [
+    req.query["warehouse_id"],
     req.query["warehouse_name"],
     req.query["rack_no"],
     req.query["cell_no"],
     req.query["row_no"],
     req.query["use_yn"],
-    req.query["warehouse_id"],
   ];
 
-  runQuery(res, sql, params);
+  runProcedure(res, sql, params);
 });
 
 // 삭제
 router.delete("/delete", (req, res) => {
-  const sql = "DELETE FROM tb_warehouse WHERE warehouse_id=?";
-  const params = [req.query["warehouse_id"]];
+  const sql = `CALL usp_warehouse_DEL (${new Array(1).fill("?").toString()})`;
+  const params = [
+    req.query["warehouse_id"],
+  ];
 
-  runQuery(res, sql, params);
+  runProcedure(res, sql, params);
 });
-
 module.exports = router;
