8c7ea38757fcca9412b6d3f762f4eceeb11ce62d
JS <js_hwang@samter.onmicrosoft.com>
2022-03-03 15:47:18 +0900

    InputBool 오류 수정

--- src/components/Input/InputBool.tsx
+++ src/components/Input/InputBool.tsx
@@ -1,14 +1,22 @@
 import { Switch } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 export default function InputBool(props: {
   onChange: (value: boolean) => void;
   isDefaultTrue?: boolean;
   isDisabled?: boolean;
 }) {
+  const [value, setValue] = useState<boolean | undefined>(props.isDefaultTrue);
+
+  useEffect(() => setValue(props.isDefaultTrue), [props]);
+
   return (
     <Switch
-      onChange={(event) => props.onChange(event.target.checked)}
-      defaultChecked={props.isDefaultTrue ? true : false}
+      onChange={(event) => {
+        setValue(event.target.checked);
+        props.onChange(event.target.checked);
+      }}
+      isChecked={value}
       isDisabled={props.isDisabled}
       size={"lg"}
     />
